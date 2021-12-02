bdffc524973155fa48c74b5a163e7eea25ec97a6
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-12-02 18:00:30 +0100

    add package to generate file

--- generator/src/main/kotlin/com/algolia/instantsearch/telemetry/generator/Main.kt
+++ generator/src/main/kotlin/com/algolia/instantsearch/telemetry/generator/Main.kt
@@ -5,7 +5,10 @@ import kotlinx.serialization.protobuf.schema.ProtoBufSchemaGenerator
 import java.io.File
 
 fun main() {
-    val proto = ProtoBufSchemaGenerator.generateSchemaText(rootDescriptor = Schema.serializer().descriptor)
+    val proto = ProtoBufSchemaGenerator.generateSchemaText(
+        rootDescriptor = Schema.serializer().descriptor,
+        packageName = Schema::class.java.packageName
+    )
     val fileName = "telemetry.proto"
     val protoFile = File(fileName)
     protoFile.printWriter().use { out -> out.write(proto) }
