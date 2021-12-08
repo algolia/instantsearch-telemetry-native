f69369ed31fd99477472ffcaf5f613efcbe7f305
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-12-08 17:02:44 +0100

    refact: `Opt` as interface (#6)

---
+++ telemetry/src/commonMain/kotlin/com/algolia/instantsearch/telemetry/Opt.kt
@@ -0,0 +1,12 @@
+package com.algolia.instantsearch.telemetry
+
+/**
+ * Flags to Opt-(in/out).
+ */
+public interface Opt {
+
+    /**
+     * Flag to enable/disable telemetry operations.
+     */
+    public var enabled: Boolean
+}
