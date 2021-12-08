11ebca2c6f2fcc097dc2e19b6fc0c78e556fe324
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-12-08 17:01:57 +0100

    add kdoc

--- telemetry/src/commonMain/kotlin/com/algolia/instantsearch/telemetry/Opt.kt
+++ telemetry/src/commonMain/kotlin/com/algolia/instantsearch/telemetry/Opt.kt
@@ -1,9 +1,12 @@
 package com.algolia.instantsearch.telemetry
 
+/**
+ * Flags to Opt-(in/out).
+ */
 public interface Opt {
 
     /**
-     * Flag to enable/disable telemetry tracing.
+     * Flag to enable/disable telemetry operations.
      */
     public var enabled: Boolean
 }
