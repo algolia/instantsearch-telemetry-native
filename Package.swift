ed20b10b3997ca7a42fa697d8738bf9af0c7cc96
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-05-17 13:52:26 +0200

    update package dep

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
     dependencies: [
       .package(name: "SwiftProtobuf",
                url: "https://github.com/apple/swift-protobuf.git",
-               from: "1.19.0"),
-      //.revision("b9ec2c47e74f6bcf00af27772818ee034a6c6c25")
+               .revision("b9ec2c47e74f6bcf00af27772818ee034a6c6c25")),
       .package(name: "Gzip",
                url: "https://github.com/1024jp/GzipSwift",
                from: "5.2.0")
