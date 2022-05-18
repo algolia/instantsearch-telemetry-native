a963d1287f3a01ed028dcee8f85b2d360820c529
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-05-19 00:37:53 +0200

    replace Gzip with self-made implementation using Compression

--- Package.swift
+++ Package.swift
@@ -14,14 +14,11 @@ let package = Package(
       .package(name: "SwiftProtobuf",
                url: "https://github.com/apple/swift-protobuf.git",
                .revision("b9ec2c47e74f6bcf00af27772818ee034a6c6c25")),
-      .package(name: "Gzip",
-               url: "https://github.com/1024jp/GzipSwift",
-               from: "5.2.0")
     ],
     targets: [
         .target(
             name: "InstantSearchTelemetry",
-            dependencies: ["SwiftProtobuf", "Gzip"]),
+            dependencies: ["SwiftProtobuf"]),
         .testTarget(
             name: "InstantSearchTelemetryTests",
             dependencies: ["InstantSearchTelemetry"]),
