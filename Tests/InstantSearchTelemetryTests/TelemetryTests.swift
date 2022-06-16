be01d0a6807776afb880266da3173c82236e082d
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-06-16 14:32:48 +0200

    add string decoding test

--- Tests/InstantSearchTelemetryTests/TelemetryTests.swift
+++ Tests/InstantSearchTelemetryTests/TelemetryTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import InstantSearchTelemetry
 
 final class TelemetryTests: XCTestCase {
-
+  
   func testEncodeDecode() throws {
     let schema = TelemetrySchema.with {
       $0.components = TelemetryComponentType.allCases.map { type in
@@ -14,7 +14,7 @@ final class TelemetryTests: XCTestCase {
       }
     }
     let data = try schema.serializedData()
-    let compressedData = try data.gzipped()
+    let compressedData = data.gzipped()
     let uncompressedData = try compressedData.gunzipped()
     let decodedSchema = try TelemetrySchema(serializedData: uncompressedData)
     XCTAssertEqual(schema, decodedSchema)
@@ -32,5 +32,43 @@ final class TelemetryTests: XCTestCase {
     telemetry.trace(type: .facetList, parameters: .facets)
     XCTAssertTrue(telemetry.components.isEmpty)
   }
-
+  
+  func testStringDecoding() throws {
+    let rawTelemetryStrings = [
+      "H4sIAAAAAAAAE3ukLXRAlfEECDGdUGU7oap1QZXxkTbbAVXWC6oMj7Q5D6hynFDVBgsC2YJICoTBCoAMEaAIABhKRiRIAAAA",
+      "H4sIAAAAAAAAAHukzXZAlfGCKsMjEEMExmCFMTguqDICAIzMnTMkAAAA",
+      "H4sIAAAAAAAAAHukzXZAlfGCKsMjEEMExhC+oMoIAGuHg1AbAAAA",
+      "H4sIAAAAAAAAE3ukzXZAlfmCKgMAAAIDWgkAAAA=",
+      "H4sIAAAAAAAAE3ukzX9AlfEECDGdUGW7oMrwSJvtgCormMFzQJXjhCr7CVUJmLggjCECZAAAlZEs0zwAAAA=",
+      "H4sIAAAAAAAAAHukzXZAlfWCKsMjEIMRzOA8oMpxQlXigiojAP1WSJgeAAAA",
+      "H4sIAAAAAAAAE3ukzXZAlfWCKgMA3F1ofwkAAAA=",
+      "H4sIAAAAAAAAE3ukzX9AlfEECDGdUGW7oMrwSJvtgCormMFzQJXthCrnCVXJC6qMj7Q5D6hynFDVBrOBajhhigVhDCGYlDCMIQJkAADFQJdBYwAAAA==",
+      "H4sIAAAAAAAAAHukzXZAlfGCKsMjEEMExhC+oMoIZrCCRTgPqHKcUJUACgIAjBiI0jAAAAA=",
+    ]
+    
+    for (index, string) in rawTelemetryStrings.enumerated() {
+      
+      guard let compressedData = Data(base64Encoded: string) else {
+        XCTFail("Base64 decoding failed: \(index)")
+        return
+      }
+      
+      let uncompressedData: Data
+      do {
+        uncompressedData = try compressedData.gunzipped()
+      } catch let unzipError {
+        XCTFail("Unzip failed  [\(index)]: \(unzipError)")
+        return
+      }
+      
+      do {
+        _ = try TelemetrySchema(serializedData: uncompressedData)
+      } catch let deserializationError {
+        XCTFail("Telemetry decoding failed [\(index)]: \(deserializationError)")
+        return
+      }
+      
+    }
+  }
+  
 }
